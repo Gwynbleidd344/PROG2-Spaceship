00cca8fecc7bc4089136cc12f907f161a52af89a
Thiann <hei.thiann.7@gmail.com>
2025-07-16 10:57:58 +0300

    feat : adding Spaceship

--- src/main/java/StaceShip.java
+++ src/main/java/StaceShip.java
@@ -1,3 +1,20 @@
-public class StaceShip {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+class SpaceShip {
+    private int x;
+    private int y;
+
+
+    public void move(String direction, int gridSize) {
+        switch (direction) {
+            case "H": if (y > 0) y--; break;
+            case "B": if (y < gridSize - 1) y++; break;
+            case "G": if (x > 0) x--; break;
+            case "D": if (x < gridSize - 1) x++; break;
+            default: System.out.println("Mouvement invalide !");
+        }
+    }
 }
