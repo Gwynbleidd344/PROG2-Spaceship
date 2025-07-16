6a8281102d7237053c4f2a977f2ed9e5fc3f819d
Vaniaina <hei.vaniaina@gmail.com>
2025-07-16 11:14:50 +0300

    fix : staceship - spaceship

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,2 +1,24 @@
 public class Grid {
+    int gridSize;
+    char[][] grid;
+    char EMPTY = '.';
+    char DEBRIS = 'J';
+    char SHIP = 'S';
+    int debrisRemaining;
+
+    public SpaceGrid(int size, int debrisRemaining, SpaceShip ship) {
+        this.gridSize = size;
+        this.debrisRemaining = debrisRemaining;
+        this.grid = new char[size][size];
+    }
+
+    public void initializeGrid() {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = EMPTY;
+            }
+        }
+        Random random = new Random();
+        int placeDebris = 0;
+    }
 }
